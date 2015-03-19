2ebcebdab6213e7f97f60ee99d3ee5123443c551
mike7549 <csap8796@student.uibk.ac.at>
2015-03-19 17:35:38 +0100

    Updated Location IBK

--- app/src/main/java/client/smrtms/com/smrtms_client/MapsActivity.java
+++ app/src/main/java/client/smrtms/com/smrtms_client/MapsActivity.java
@@ -60,6 +60,6 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(47.267222, 11.392778)).title("Marker"));
     }
 }
