c813ae922152125fd6814bfd353b2e512d9b1ab5
Maximilian Siess <hunterwolf0815@gmail.com>
2015-04-27 17:11:48 +0200

    [Server] Added Apache HTTP depedencies & started to add http code to main

--- SMRTMS_Server/src/server/Main.java
+++ SMRTMS_Server/src/server/Main.java
@@ -3,14 +3,22 @@ package server;
 import static jooqdb.Tables.*;
 import static org.jooq.impl.DSL.*;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.zip.GZIPInputStream;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
+import org.jooq.tools.json.JSONObject;
 
 /*
  *  Prepare the SQL Server
@@ -21,6 +29,20 @@ import org.jooq.impl.DSL;
 
 
 public class Main {
+	
+	private String convertStreamToString(InputStream is) {
+        String line = "";
+        StringBuilder total = new StringBuilder();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        try {
+            while ((line = rd.readLine()) != null) {
+                total.append(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Could not convert Stream to String: " + e.toString());
+        }
+        return total.toString();
+    }
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
@@ -47,6 +69,23 @@ public class Main {
 		// Simple exception handling
 		catch (Exception e) {
 			e.printStackTrace();
+		}
+		
+		HttpEntity resultentity = HttpResponse.getEntity();
+		InputStream inputstream = resultentity.getContent();
+		Header contentencoding = HttpResponse.getFirstHeader("Content-Encoding");
+		
+		while(true) {
+			
+			if(contentencoding != null && contentencoding.getValue().equalsIgnoreCase("gzip")) {
+				inputstream = new GZIPInputStream(inputstream);
+			}
+			String resultstring = convertStreamToString(inputstream);
+			inputstream.close();
+			resultstring = resultstring.substring(1,resultstring.length()-1);
+			recvdref.setText(resultstring + "\n\n" + httppostreq.toString().getBytes());
+			JSONObject recvdjson = new JSONObject(resultstring);
+			recvdref.setText(recvdjson.toString(2));
 		}
 	}
 
