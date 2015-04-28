e71f5dda1e09a4a1ba8e7bf258f4306386bb0d61
HunterwolfAT <hunterwolf0815@gmail.com>
2015-04-28 11:51:52 +0200

    ConnectionManager Setup without errors

--- app/src/main/java/client/smrtms/com/smrtms_client/controller/ConnectionManager.java
+++ app/src/main/java/client/smrtms/com/smrtms_client/controller/ConnectionManager.java
@@ -4,27 +4,53 @@ import android.widget.Toast;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.zip.GZIPInputStream;
+
+import client.smrtms.com.smrtms_client.activity.MainScreen;
 
 public class ConnectionManager {
     public void SendFile() {
-        HttpEntity resultentity = httpresponse.getEntity();
-        InputStream inputstream = resultentity.getContent();
-        Header contentencoding = httpresponse.getFirstHeader("Content-Encoding");
-        if(contentencoding != null && contentencoding.getValue().equalsIgnoreCase("gzip")) {
-            inputstream = new GZIPInputStream(inputstream);
+        JSONObject jsonobj = new JSONObject();
+
+        try {
+            final String wurl = "sepm@phil-m.eu:8080";
+
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppostreq = new HttpPost(wurl);
+            StringEntity se = new StringEntity(jsonobj.toString());
+            se.setContentType("application/json;charset=UTF-8");
+            se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json;charset=UTF-8"));
+            httppostreq.setEntity(se);
+            HttpResponse httpresponse = httpclient.execute(httppostreq);
+
+            HttpEntity resultentity = httpresponse.getEntity();
+            InputStream inputstream = resultentity.getContent();
+            Header contentencoding = httpresponse.getFirstHeader("Content-Encoding");
+            if (contentencoding != null && contentencoding.getValue().equalsIgnoreCase("gzip")) {
+                inputstream = new GZIPInputStream(inputstream);
+            }
+            String resultstring = convertStreamToString(inputstream);
+
+            inputstream.close();
+
+            resultstring = resultstring.substring(1, resultstring.length() - 1);
+           // recvdref.setText(resultstring + "\n\n" + httppostreq.toString().getBytes());
+            JSONObject recvdjson = new JSONObject(resultstring);
+           // recvdref.setText(recvdjson.toString(2));
         }
-        String resultstring = convertStreamToString(inputstream);
-        inputstream.close();
-        resultstring = resultstring.substring(1,resultstring.length()-1);
-        recvdref.setText(resultstring + "\n\n" + httppostreq.toString().getBytes());
-        JSONObject recvdjson = new JSONObject(resultstring);
-        recvdref.setText(recvdjson.toString(2));
+        catch (Exception e) { e.printStackTrace(); }
     }
 
     private String convertStreamToString(InputStream is) {
@@ -36,7 +62,7 @@ public class ConnectionManager {
                 total.append(line);
             }
         } catch (Exception e) {
-            Toast.makeText(this, "Stream Exception", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(THE_CONTEXT_OF_THIS_FREAKIN_APP , "Stream Exception", Toast.LENGTH_SHORT).show();
         }
         return total.toString();
     }
