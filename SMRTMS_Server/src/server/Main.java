262c58da367149be7595e661deac261bb85b217b
Maximilian Siess <hunterwolf0815@gmail.com>
2015-05-04 11:16:56 +0200

    Init server logic

--- SMRTMS_Server/src/server/Main.java
+++ SMRTMS_Server/src/server/Main.java
@@ -6,15 +6,12 @@ import static org.jooq.impl.DSL.*;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.zip.GZIPInputStream;
 
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
@@ -31,20 +28,6 @@ import org.jooq.tools.json.JSONObject;
 
 
 public class Main {
-	
-	private static String convertStreamToString(InputStream is) {
-        String line = "";
-        StringBuilder total = new StringBuilder();
-        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-        try {
-            while ((line = rd.readLine()) != null) {
-                total.append(line);
-            }
-        } catch (Exception e) {
-            System.out.println("Could not convert Stream to String: " + e.toString());
-        }
-        return total.toString();
-    }
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
@@ -70,6 +53,14 @@ public class Main {
 		
 		// Simple exception handling
 		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Do the server stuff
+		try {
+			Server server = new Server(8887);
+		}
+		catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 		
