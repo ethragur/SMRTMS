1b59f4b8911d573bb88cbe5da79efd00423cebff
HunterwolfAT <hunterwolf0815@gmail.com>
2015-05-11 12:48:13 +0200

    Changed Tokens to be usable

--- app/src/main/java/client/smrtms/com/smrtms_client/tokens/UserUpdateToken.java
+++ app/src/main/java/client/smrtms/com/smrtms_client/tokens/UserUpdateToken.java
@@ -5,16 +5,9 @@ package client.smrtms.com.smrtms_client.tokens;
  */
 public class UserUpdateToken extends Token
 {
-    Double Latitude;
-    Double Longitude;
-    String UserID;
-
-    public UserUpdateToken(Double lat, Double longi, String ID)
+    public UserUpdateToken()
     {
         super("UserUpdate");
-        Latitude = lat;
-        Longitude = longi;
-        UserID = ID;
     }
 
 }
