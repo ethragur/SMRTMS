388bbb3371bf5d86a4253c0c9dc80d9f4677d94e
Maximilian Siess <hunterwolf0815@gmail.com>
2015-05-11 09:14:54 +0200

    [Server] Fleshing out DBManager

--- SMRTMS_Server/src/server/AuthenticationManager.java
+++ SMRTMS_Server/src/server/AuthenticationManager.java
@@ -1,8 +1,11 @@
 package server;
 
 import client.smrtms.com.smrtms_client.tokens.AuthenticationToken;
+import server.DBManager;
 
 public class AuthenticationManager {
+	
+	private DBManager dbm = new DBManager();
 	
 	public boolean AuthenticateUser( AuthenticationToken t ) {
 		boolean result = false;
--- SMRTMS_Server/src/server/DBManager.java
+++ SMRTMS_Server/src/server/DBManager.java
@@ -4,6 +4,7 @@ import static jooqdb.Tables.USER;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import org.jooq.DSLContext;
 import org.jooq.Record;
@@ -18,23 +19,30 @@ public class DBManager {
 	
 	public void bla () {
 		// Connection is the only JDBC recource that we need
-				// PreparedStatement and ResultSet are handled by jOOQ, internally
-				try (Connection conn = DriverManager.getConnection(url, userName, password)) {
-					DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
-					Result<Record> result = create.select().from(USER).fetch();
-					
-					for (Record r : result) {
-						Integer id = r.getValue(USER.ID);
-						String firstName = r.getValue(USER.FIRST_NAME);
-						String lastName = r.getValue(USER.LAST_NAME);
-						
-						System.out.println("ID: " + id + " first name: " + firstName + " last name: " + lastName);
-					}
-				}
-				
-				// Simple exception handling
-				catch (Exception e) {
-					e.printStackTrace();
-				}
+		// PreparedStatement and ResultSet are handled by jOOQ, internally
+		DSLContext create = getConnection();
+			
+		Result<Record> result = create.select().from(USER).fetch();
+		
+		for (Record r : result) {
+			Integer id = r.getValue(USER.ID);
+			String firstName = r.getValue(USER.FIRST_NAME);
+			String lastName = r.getValue(USER.LAST_NAME);
+			
+			System.out.println("ID: " + id + " first name: " + firstName + " last name: " + lastName);
+		}
+
+	}
+	
+	private DSLContext getConnection() {
+		try {
+			Connection conn = DriverManager.getConnection(url, userName, password);
+			return DSL.using(conn, SQLDialect.MYSQL);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 }
