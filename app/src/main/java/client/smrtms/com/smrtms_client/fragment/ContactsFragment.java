01079624170595d650b258c11ffe00c49dc32215
RaphaelHub <raphael-gruber@hotmail.com>
2015-05-16 21:28:42 +0200

    Changed to RelativeLayout, add Map button (no functionality)

--- app/src/main/java/client/smrtms/com/smrtms_client/fragment/ContactsFragment.java
+++ app/src/main/java/client/smrtms/com/smrtms_client/fragment/ContactsFragment.java
@@ -1,14 +1,17 @@
 package client.smrtms.com.smrtms_client.fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
 
 
 import com.firebase.androidchat.ChatActivity;
@@ -16,9 +19,9 @@ import com.firebase.androidchat.ChatActivity;
 import java.util.ArrayList;
 
 import client.smrtms.com.smrtms_client.R;
-import client.smrtms.com.smrtms_client.activity.RegisterActivity;
 import client.smrtms.com.smrtms_client.controller.LoginUser;
 import client.smrtms.com.smrtms_client.controller.User;
+
 
 
 public class ContactsFragment extends Fragment {
@@ -36,21 +39,27 @@ public class ContactsFragment extends Fragment {
 
         super.onViewCreated(view, savedInstanceState);
 
-        // This will create the LinearLayout
-        LinearLayout ll = new LinearLayout(getActivity());
-        ll.setOrientation(LinearLayout.VERTICAL);
+        // This will create the LinearLayout Vertical
+        RelativeLayout layout = new RelativeLayout(getActivity());
+        layout.setLayoutParams(new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
 
-        // Configuring the width and height of the linear layout.
-        LinearLayout.LayoutParams llLP = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        ll.setLayoutParams(llLP);
+        // Getting size of the display
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager wm = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(metrics);
+        final float width = metrics.widthPixels;
 
-        // Configuring the width and height of the buttons
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
+
+        // create Buttons
+        ArrayList<Button> fButton = new ArrayList<>();
+        ArrayList<Button> mButton = new ArrayList<>();
+        int id = 1;
         for(final User friend: LoginUser.getInstance().getFriendList()) {
+            // Friend Button
             Button temp = new Button(getActivity());
             temp.setText(friend.getUsername());
-            temp.setLayoutParams(lp);
+            temp.setId(id++);
             temp.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -59,16 +68,51 @@ public class ContactsFragment extends Fragment {
                     getActivity().startActivity(myIntent);
                 }
             });
-            ll.addView(temp);
+            fButton.add(temp);
+
+            // Map Button
+            temp = new Button(getActivity());
+            temp.setText("Map");
+            temp.setId(id++);
+            mButton.add(temp);
+        }
+
+        boolean firstButton = true; //button on the top
+        // set layout to the buttons
+        for(int i = 0; i < fButton.size(); i++) {
+            // Configuring the width and height of the Friend buttons and Map buttons
+            RelativeLayout.LayoutParams paramFriend = new RelativeLayout.LayoutParams((int) (width - width/3),RelativeLayout.LayoutParams.WRAP_CONTENT);
+            RelativeLayout.LayoutParams paramMap = new RelativeLayout.LayoutParams((int) (width/3),RelativeLayout.LayoutParams.WRAP_CONTENT);
+            paramFriend.addRule(RelativeLayout.ALIGN_PARENT_LEFT,RelativeLayout.TRUE);
+            paramMap.addRule(RelativeLayout.ALIGN_PARENT_RIGHT,RelativeLayout.TRUE);
+
+            if (firstButton) {
+                fButton.get(i).setLayoutParams(paramFriend);
+                mButton.get(i).setLayoutParams(paramMap);
+                firstButton = false;
+            } else {
+                paramFriend.addRule(RelativeLayout.BELOW,fButton.get(i-1).getId());
+                fButton.get(i).setLayoutParams(paramFriend);
+                System.out.println(fButton.get(i).getText() +" is under" + fButton.get(i-1).getText());
+                paramMap.addRule(RelativeLayout.BELOW,mButton.get(i-1).getId());
+                mButton.get(i).setLayoutParams(paramMap);
+            }
+
+            layout.addView(fButton.get(i));
+            layout.addView(mButton.get(i));
         }
         //add friend button
         Button addFriend = new Button(getActivity());
         addFriend.setText("+ Friend");
-        addFriend.setLayoutParams(lp);
-        ll.addView(addFriend);
+        RelativeLayout.LayoutParams paramAdd = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+        paramAdd.addRule(RelativeLayout.BELOW,fButton.get(fButton.size()-1).getId());
+        addFriend.setLayoutParams(paramAdd);
+        layout.addView(addFriend);
 
 
         ViewGroup viewGroup = (ViewGroup) view;
-        viewGroup.addView(ll);
+        viewGroup.addView(layout);
+
+
     }
 }
