a36d993246329886e36992cd88955d25384b2a7b
RaphaelHub <raphael-gruber@hotmail.com>
2015-05-16 19:21:23 +0200

    Event create button

--- app/src/main/java/client/smrtms/com/smrtms_client/fragment/EventFragment.java
+++ app/src/main/java/client/smrtms/com/smrtms_client/fragment/EventFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import client.smrtms.com.smrtms_client.R;
 
@@ -19,4 +21,32 @@ public class EventFragment extends Fragment {
 
         return rootView;
     }
-}
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+        // This will create the LinearLayout
+        LinearLayout ll = new LinearLayout(getActivity());
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        // Configuring the width and height of the linear layout.
+        LinearLayout.LayoutParams llLP = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        ll.setLayoutParams(llLP);
+
+        // Configuring the width and height of the buttons
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+
+        //add friend button
+        Button addFriend = new Button(getActivity());
+        addFriend.setText("+ Event");
+        addFriend.setLayoutParams(lp);
+        ll.addView(addFriend);
+
+
+        ViewGroup viewGroup = (ViewGroup) view;
+        viewGroup.addView(ll);
+    }
+    }
