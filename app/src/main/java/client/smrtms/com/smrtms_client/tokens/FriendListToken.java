f592300fe506b08ac946c7168d2627d56c0e07bf
ethragur <stefan.kuhnert94@gmail.com>
2015-05-25 16:20:37 +0200

    [Client][FEATURE] Implemented FriendList Token
    Server should do this too!

---
+++ app/src/main/java/client/smrtms/com/smrtms_client/tokens/FriendListToken.java
@@ -0,0 +1,18 @@
+package client.smrtms.com.smrtms_client.tokens;
+
+import java.util.List;
+
+import client.smrtms.com.smrtms_client.controller.User;
+
+/**
+ * Created by effi on 5/25/15.
+ */
+public class FriendListToken extends Token
+{
+    public List<User> userList;
+    public FriendListToken()
+    {
+        super("FriendList");
+    }
+
+}
