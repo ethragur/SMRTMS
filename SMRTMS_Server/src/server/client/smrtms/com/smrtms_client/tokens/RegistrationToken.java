4af87b4055df652fa4b1a0b9eb1fdeab6eecaec3
ethragur <stefan.kuhnert94@gmail.com>
2015-06-07 23:42:17 +0200

    gradle integration for server, everything crashes when build

---
+++ SMRTMS_Server/src/server/client/smrtms/com/smrtms_client/tokens/RegistrationToken.java
@@ -0,0 +1,12 @@
+package client.smrtms.com.smrtms_client.tokens;
+
+/**
+ * Created by effi on 4/29/15.
+ */
+public class RegistrationToken extends Token
+{
+    public RegistrationToken()
+    {
+        super("Registration");
+    }
+}
---
+++ app/src/test/java/ConnectionTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by effi on 6/7/15.
+ */
+public class ConnectionTest
+{
+    @Test
+    public void testSendingAndReceiving()
+    {
+
+    }
+}
