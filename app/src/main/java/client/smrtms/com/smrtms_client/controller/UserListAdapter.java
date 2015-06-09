80146a9759dc12e8302470ef6b62455116f6b77d
RaphaelHub <raphael-gruber@hotmail.com>
2015-06-09 21:45:28 +0200

    Bugfix - change lat and log in calculateDistance()

--- app/src/main/java/client/smrtms/com/smrtms_client/controller/UserListAdapter.java
+++ app/src/main/java/client/smrtms/com/smrtms_client/controller/UserListAdapter.java
@@ -49,7 +49,7 @@ public class UserListAdapter extends ArrayAdapter<User>{
         }
 
         viewHolder.name.setText(item.getUsername());
-        viewHolder.coordinates.setText("Distance: " + LoginUser.getInstance().getServerTask().getGpsTracker().calculateDistance(item.getLongitude(),item.getLatitude())+ "km") ;
+        viewHolder.coordinates.setText("Distance: " + LoginUser.getInstance().getServerTask().getGpsTracker().calculateDistance(item.getLatitude(),item.getLongitude())+ "km") ;
         return convertView;
     }
 }
