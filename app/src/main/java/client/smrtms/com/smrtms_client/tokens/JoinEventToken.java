af364740f2922f25c5bffd6bdd63fb77b50c8e31
ethragur <stefan.kuhnert94@gmail.com>
2015-06-10 00:04:23 +0200

    [Client] Added JoinEvent token

--- app/src/main/java/client/smrtms/com/smrtms_client/tokens/JoinEventToken.java
+++ app/src/main/java/client/smrtms/com/smrtms_client/tokens/JoinEventToken.java
@@ -1,8 +1,17 @@
 package client.smrtms.com.smrtms_client.tokens;
 
+import client.smrtms.com.smrtms_client.controller.LoginUser;
+
 /**
  * Created by effi on 6/9/15.
  */
-public class JoinEventToken
+public class JoinEventToken extends Token
 {
+    String EventName;
+    public JoinEventToken(String EventName)
+    {
+        super("JoinEvent", LoginUser.getInstance().getID());
+        this.EventName = EventName;
+    }
+
 }
