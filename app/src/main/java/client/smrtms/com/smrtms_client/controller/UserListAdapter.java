97aa586fd9f7cbdfbcff4d9ad8cadbd787958a54
RaphaelHub <raphael-gruber@hotmail.com>
2015-06-09 21:36:53 +0200

    calculate distance between you and friend

--- app/src/main/java/client/smrtms/com/smrtms_client/controller/UserListAdapter.java
+++ app/src/main/java/client/smrtms/com/smrtms_client/controller/UserListAdapter.java
@@ -49,8 +49,7 @@ public class UserListAdapter extends ArrayAdapter<User>{
         }
 
         viewHolder.name.setText(item.getUsername());
-        viewHolder.coordinates.setText("Coordinates: " + item.getLongitude() + " " + item.getLatitude());
-
+        viewHolder.coordinates.setText("Distance: " + LoginUser.getInstance().getServerTask().getGpsTracker().calculateDistance(item.getLongitude(),item.getLatitude())+ "km") ;
         return convertView;
     }
 }
