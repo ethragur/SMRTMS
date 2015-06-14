3af10dcc71c9806c5a935af718c38a22db0161d9
ethragur <stefan.kuhnert94@gmail.com>
2015-06-14 21:58:13 +0200

    Update Tests

--- app/src/test/java/LoginTest.java
+++ app/src/test/java/LoginTest.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import client.smrtms.com.smrtms_client.activity.LoginActivity;
+import client.smrtms.com.smrtms_client.controller.JSONParser;
+import client.smrtms.com.smrtms_client.controller.ServerControl;
+import client.smrtms.com.smrtms_client.tokens.AuthenticationToken;
 
 /**
  * Created by effi on 4/20/15.
@@ -22,7 +25,16 @@ public class LoginTest extends ActivityInstrumentationTestCase2<LoginActivity>
     @Test
     public void testLogin()
     {
+        AuthenticationToken testAToken = new AuthenticationToken("test@test.com", "test");
+        testAToken.access = true;
+        JSONParser<AuthenticationToken> testParser = new JSONParser<>();
+        String testInput = testParser.JSONWriter(testAToken);
 
+        assertTrue(testAToken.email.equals(testParser.readJson(testInput, AuthenticationToken.class).email));
+
+        ServerControl testController = new ServerControl(testInput, testAToken);
+
+        assertTrue(ServerControl.gotAuthToken);
     }
 
 }
--- app/src/test/java/ServerControlTest.java
+++ app/src/test/java/ServerControlTest.java
@@ -1,47 +1,66 @@
+import android.content.Context;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import client.smrtms.com.smrtms_client.controller.GPSTracker;
 import client.smrtms.com.smrtms_client.controller.JSONParser;
+import client.smrtms.com.smrtms_client.controller.LoginUser;
 import client.smrtms.com.smrtms_client.controller.ServerControl;
 import client.smrtms.com.smrtms_client.tokens.AuthenticationToken;
+import client.smrtms.com.smrtms_client.tokens.FriendReqToken;
 
 /**
  * Created by effi on 6/9/15.
  */
 public class ServerControlTest extends TestCase
 {
-    @Mock
     ServerControl testController;
+
+    @Mock
+    Context t;
+
+    @Mock
+    GPSTracker gpsTracker;
 
    @Test
    public void testMethodExecutions()
    {
-        testController = Mockito.mock(ServerControl.class);
-        AuthenticationToken testAToken = new AuthenticationToken("test@test.com", "test");
+       try {
+           t = Mockito.mock(Context.class);
+           gpsTracker = Mockito.mock(GPSTracker.class);
+           LoginUser.createInstance("test@test.com", "", t);
 
-        JSONParser<AuthenticationToken> testParser = new JSONParser<>();
-        String testInput = testParser.JSONWriter(testAToken);
-        assertTrue(testAToken.email.equals(testParser.readJson(testInput, AuthenticationToken.class).email));
-        testController.input = testInput;
-        testController.t = testAToken;
+       }catch(Throwable e)
+       {
+           assertEquals(NullPointerException.class, e.getClass());
+           //do nothing, catching first one because of Android init
+       }
+       LoginUser.getInstance().serverTask.setGpsTracker(gpsTracker);
+       AuthenticationToken testAToken = new AuthenticationToken("test@test.com", "test");
+       testAToken.access = true;
+       JSONParser<AuthenticationToken> testParser = new JSONParser<>();
+       String testInput = testParser.JSONWriter(testAToken);
 
+       assertTrue(testAToken.email.equals(testParser.readJson(testInput, AuthenticationToken.class).email));
 
-        Thread x = new Thread(testController);
-        x.start();
-        try {
-            Thread.sleep(1000);
-        }
-        catch(Exception e)
-        {}
-        //was method called Thread called
-        Mockito.verify(testController, Mockito.times(1)).run();
-        //check if thread is finished
-        assertFalse(x.isAlive());
+       testController = new ServerControl(testInput, testAToken);
 
+       Thread x = new Thread(testController);
+       x.start();
 
+       try {
+           Thread.sleep(1000);
+       } catch (Exception e)
+       {
+       }
+
+       assertFalse(x.isAlive());
+
+       assert(LoginUser.getInstance().isLogin());
 
    }
 }
